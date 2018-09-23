dce0c50f51f9c91077c650453ae405158353bc86
Rahul Ranjan <rahul.sjce03@gmail.com>
2018-09-23 14:14:27 +0530

    initial commit

---
+++ src/main/java/com/test/jenkinsPrj/JenkinsPrjApplication.java
@@ -0,0 +1,12 @@
+package com.test.jenkinsPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPrjApplication.class, args);
+	}
+}
---
+++ src/test/java/com/test/jenkinsPrj/JenkinsPrjApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.jenkinsPrj;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JenkinsPrjApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
